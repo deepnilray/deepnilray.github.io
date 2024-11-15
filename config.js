d7ca4b630bdf17a686d4b866abf1c4e5639a56de
deepnilray <95030337+deepnilray@users.noreply.github.com>
2024-11-15 13:55:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    apiKey: 'your-api-key-here'
+    apiKey: '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'
 };
 <script src="config.js"></script>
